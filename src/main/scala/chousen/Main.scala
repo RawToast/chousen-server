5bd337d24b20419f81308ddaef0af66d0397900f
rawtoast <jim@jamesmorris.org>
2016-10-14 19:22:35 +0100

    WIP: Player choice -- fix TODO

--- src/main/scala/chousen/Main.scala
+++ src/main/scala/chousen/Main.scala
@@ -1,5 +1,6 @@
 package chousen
 
+import scala.collection.immutable.Iterable
 import scala.util.Random
 
 
@@ -8,12 +9,13 @@ object Main extends App {
   statement("Enter name: ")
   val name = scala.io.StdIn.readLine()
   val player = PlayerCharacter(name)
-  val someEnemies: List[EnemyCharacter] = List(EnemyCharacter.slime(),
-    EnemyCharacter.yellowSlime(),
-    EnemyCharacter.giantSlime())
+  val someEnemies: List[Set[BaseCharacter]] = List(/*Set(EnemyCharacter.slime()),*/
+    Set(EnemyCharacter.yellowSlime(), EnemyCharacter.slime()),
+    Set(EnemyCharacter.giantSlime()))
 
   GameLoop(name).loop(player, someEnemies)
 }
+
 
 case class State(playerAlive: Boolean, cast: Actors)
 
@@ -22,7 +24,7 @@ case class GameLoop(playerName: String) {
   story(s"It was dark and smelly")
   statement(s"Eventually $playerName finds a room with a chest!")
 
-  def loop(p: BaseCharacter, enemies: List[BaseCharacter]) = {
+  def loop(p: BaseCharacter, enemies: List[Set[BaseCharacter]]) = {
 
     // Need to place these elsewhere
     implicit val convert = (b: BaseCharacter) => Set(b)
@@ -76,11 +78,16 @@ case class GameLoop(playerName: String) {
       }
     }
 
-    def play2(player: BaseCharacter, es: List[BaseCharacter]): List[BaseCharacter] = {
+    def play(player: BaseCharacter, es: List[Set[BaseCharacter]]): List[Set[BaseCharacter]] = {
       if (es.nonEmpty) {
         // Get first enemy/enemies
         val e = es.head
-        exclaim(s"A ${e.name} appears")
+
+        if (e.size == 1) exclaim(s"A ${e.head.name} appears")
+        else {
+          e.map(en => en.name)
+          exclaim(s"${e.toString()} appears")
+        }
 
         val act = Actors(player, e) // Implicit
 
@@ -90,12 +97,12 @@ case class GameLoop(playerName: String) {
 
 
         // Conclude
-        if (result._1) play2(result._2.player, es.tail)
+        if (result._1) play(result._2.player, es.tail)
         else es
       } else Nil
     }
 
-    if (play2(p, enemies).nonEmpty) {
+    if (play(p, enemies).nonEmpty) {
       exclaim("Game over")
     } else {
       story(s"$playerName found a wooden chest")
@@ -141,7 +148,31 @@ trait Attack extends Action {
   char: BaseCharacter =>
 
   def attack(target: Set[BaseCharacter], bystanders: Option[Set[BaseCharacter]]): Actors = {
-    if (target.size != 1) throw new RuntimeException("Cannot attack multiple targets")
+    if (target.size != 1)
+      if (char.isPlayer) {
+
+        statement("Select a target:")
+
+        val targets = target.foldLeft(Map.empty[String, BaseCharacter]){
+          (m, bc: BaseCharacter) => {
+            var count = 1
+            val newMap = m.+((s"$count", bc))
+            count = count + 1
+            newMap
+          }
+        }
+        //
+        val targetString = targets.map(kv => (kv._1, kv._2.name)).flatMap(kv => s"[${kv._1}]:${kv._2}").toString()
+        statement(targetString)
+
+        val choice = scala.io.StdIn.readLine().toLowerCase
+
+        //TODO: Take choice (if exists) and produce 2nd set w/o choice and pass into def(attack)
+        targets.get(choice)
+
+
+      }
+      else throw new RuntimeException("Enemies cannot attack multiple targets")
 
     exclaim(s"${char.name} attacks")
 
