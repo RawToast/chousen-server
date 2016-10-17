1d55ddcaadd14626e5f003e19665d0c118861faa
James Morris <jim@jamesmorris.org>
2016-10-17 20:11:20 +0100

    Turn order based on Speed stat
    * Added actors test for speed calculations
    * Simple speed ordering solution

---
+++ src/main/scala/chousen/Actors.scala
@@ -0,0 +1,67 @@
+package chousen
+
+import scala.util.Random
+
+case class Actors(actor: BaseCharacter, cast: Set[BaseCharacter]) {
+
+  // Very simple turn change
+  def changeTurn: Actors = {
+
+    def calculateTurn(fc: Set[BaseCharacter]): Actors = {
+
+      val postCalcActors: Set[BaseCharacter] = fc.map {
+        // Good argument that the position increase should be one my the BC class
+        case p: PlayerCharacter => p.copy()(position = p.position + p.speed)
+        case e: EnemyCharacter => e.copy()(position = e.position + e.speed)
+      }
+
+      def actorsWith: ((BaseCharacter) => Boolean) => Set[BaseCharacter] =
+        postCalcActors.filter(_: (BaseCharacter) => Boolean)
+
+
+      def numActorsWith(f: (BaseCharacter) => Boolean) =
+        actorsWith(f)
+          .size
+
+      lazy val actorsWithPosition = actorsWith(_.hasPosition)
+      lazy val actorsHadPosition = actorsWith(_.hadPosition)
+
+
+      actorsWithPosition.size match {
+        case 0 => calculateTurn(postCalcActors)
+        case 1 =>
+          val activeActor = postCalcActors.maxBy(bc => bc.position)
+          Actors(activeActor.resetPosition, postCalcActors - activeActor)
+        case _ =>
+          val awp = actorsWithPosition.toList.sortBy(f => -f.position)
+
+          if (awp.head.position > awp.tail.head.position) Actors(awp.head.resetPosition, postCalcActors - awp.head)
+          else {
+            actorsHadPosition.size match {
+              case 0 => calculateTurn(postCalcActors)
+              case 1 =>
+                val actor = actorsHadPosition.head
+                Actors(actor.resetPosition, postCalcActors - actor)
+              case _ =>
+                val topSpeedCount = numActorsWith(bc => bc.speed == actorsWithPosition.maxBy(_.speed).speed)
+                if (topSpeedCount == 1) calculateTurn(postCalcActors)
+                else {
+                  val chosenOne = Random.shuffle(actorsWithPosition).head
+                  Actors(chosenOne.resetPosition, postCalcActors - chosenOne)
+                }
+            }
+          }
+      }
+    }
+
+    calculateTurn(fullCast)
+  }
+
+  def fullCast = cast + actor
+
+  def player = cast.find(bc => bc.isPlayer).getOrElse(actor)
+
+  def hasEnemies = !actor.isPlayer || cast.exists(!_.isPlayer)
+
+  def newLead(actor:BaseCharacter) = Actors(actor.resetPosition, fullCast - actor)
+}
--- src/main/scala/chousen/BaseCharacter.scala
+++ src/main/scala/chousen/BaseCharacter.scala
@@ -1,14 +1,20 @@
 package chousen
 
-abstract class BaseCharacter extends Nameable with Stats with Attack {
+
+sealed abstract class BaseCharacter extends Nameable with Stats with Attack {
   val isPlayer: Boolean
+  val position: Int
+
+  val hasPosition:Boolean = position >= 100
+  val hadPosition:Boolean = hasPosition && ((position - speed) >= 100)
 
   def takeDamage(damage: Int): BaseCharacter
-
 
   def deathMessage: String = {
     s"$name dies"
   }
+
+  def resetPosition: BaseCharacter
 
   override def toString: String = name
 }
@@ -19,6 +25,7 @@ case class PlayerCharacter(name: String, maxHp: Int = 100, currentHp: Int = 100,
                            override val intellect: Int = 8,
                            override val vitality: Int = 8,
                            override val speed: Int = 8)
+                          (override val position:Int = 0)
   extends BaseCharacter {
 
   override val isPlayer: Boolean = true
@@ -27,8 +34,10 @@ case class PlayerCharacter(name: String, maxHp: Int = 100, currentHp: Int = 100,
     val currHp = this.currentHp - damage
     if (currHp <= 0) story(s"$name takes $damage")
     else statement(s"$name takes $damage and has ${currHp}HP left!")
-    this.copy(currentHp = currHp)
+    this.copy(currentHp = currHp)(this.position)
   }
+
+  def resetPosition = copy()(this.position - 100)
 }
 
 case class EnemyCharacter(name: String, maxHp: Int, currentHp: Int,
@@ -36,29 +45,31 @@ case class EnemyCharacter(name: String, maxHp: Int, currentHp: Int,
                           override val dexterity: Int = 8,
                           override val intellect: Int = 8,
                           override val vitality: Int = 8,
-                          override val speed: Int = 8)
+                          override val speed: Int = 8)(
+                          override val position:Int = 0)
+
   extends BaseCharacter {
 
   override val isPlayer: Boolean = false
 
   def this(name: String, hp: Int) = {
-    this(name, hp, hp)
+    this(name, hp, hp)()
   }
 
   override def takeDamage(damage: Int): BaseCharacter = {
     val currHp = this.currentHp - damage
-    this.copy(currentHp = currHp)
+    this.copy(currentHp = currHp)(this.position)
   }
+
+  def resetPosition = copy()(this.position - 100)
 }
 
 object EnemyCharacter {
   def create(name: String, maxHp: Int) = new EnemyCharacter(name, maxHp)
 
-  def slime() = EnemyCharacter("Slime", 10, 10, strength = 4)
+  def slime() = EnemyCharacter("Slime", 10, 10, strength = 4, speed = 7)()
 
-  def yellowSlime() = EnemyCharacter("Yellow Slime", 15, 15, vitality = 2)
+  def yellowSlime() = EnemyCharacter("Yellow Slime", 15, 15, vitality = 2)()
 
-  def giantSlime() = EnemyCharacter("Giant Slime", 30, 30, strength = 18, vitality = 12)
+  def giantSlime() = EnemyCharacter("Giant Slime", 30, 30, strength = 18, vitality = 12, speed = 6)()
 }
-
-
