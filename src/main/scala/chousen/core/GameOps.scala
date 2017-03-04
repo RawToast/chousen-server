42e69b1d1abe7c44bfcaf89f2f5f53aefb539ed0
rawtoast <jim@jamesmorris.org>
2017-03-04 18:04:51 +0000

    GameOps handles basic updates

--- src/main/scala/chousen/core/GameOps.scala
+++ src/main/scala/chousen/core/GameOps.scala
@@ -2,8 +2,49 @@ package chousen.core
 
 import api.data.{Enemy, GameMessage, Player}
 
+import scala.annotation.tailrec
+
 
 object GameOps {
-  def update(speed10Player: Player, enemies: Set[Enemy], emptyMessages: Seq[GameMessage]):
-  (Player, Set[Enemy], Seq[GameMessage]) = ???
+  type Actors = (Player, Set[Enemy])
+  type EncounterData = (Player, Set[Enemy], Seq[GameMessage])
+  type EncounterUpdate = ((Player, Set[Enemy], Seq[GameMessage])) => (Player, Set[Enemy], Seq[GameMessage])
+
+  def update(player: Player, enemies: Set[Enemy], messages: Seq[GameMessage]):
+  (Player, Set[Enemy], Seq[GameMessage]) = {
+
+    def process: EncounterUpdate = ensureActive _ andThen announceActive
+
+    process(Tuple3(player, enemies, messages))
+  }
+
+  @tailrec
+  def ensureActive(encounterData: EncounterData): EncounterData = {
+    val (p, es, msgs) = encounterData
+
+    val (player, enemies) = p.copy(position = p.position + p.stats.speed) ->
+      es.map(e => e.copy(position = e.position + e.stats.speed))
+
+    val numWithPosition = if (player.position >= 100) 1 + enemies.count(_.position >= 100)
+    else enemies.count(_.position >= 100)
+
+    numWithPosition match {
+      case 0 => ensureActive(Tuple3(player, enemies, msgs))
+      case 1 => (player, enemies, msgs)
+      case _ => (player, enemies, msgs) //TODO handle
+    }
+  }
+
+  private def announceActive(encounterData: EncounterData): EncounterData = {
+    val (player, enemies, msgs) = encounterData
+
+    val fastestEnemy = enemies.maxBy(_.position)
+
+    val message = {
+      if (player.position > fastestEnemy.position) GameMessage(s"${player.name}'s turn!")
+      else GameMessage(s"${fastestEnemy.name}'s turn!")
+    }
+
+    (player, enemies, msgs :+ message)
+  }
 }
--- src/test/scala/chousen/core/GameOpsSpec.scala
+++ src/test/scala/chousen/core/GameOpsSpec.scala
@@ -39,6 +39,77 @@ class GameOpsSpec extends WordSpec {
 
     }
 
+
+    "provided two a player and enemy with equal speeds and different positions" should {
+
+      // Note that the enemy is closer to their next turn
+      val player = Player("Player", speed10Char, position = 0)
+      val enemy = Enemy("Quick Enemy", UUID.randomUUID(), speed10Char, position = 50)
+      val emptyMessages = Seq.empty[GameMessage]
+
+      val (updPlayer, updEnemies, updMessages) =
+        GameOps.update(player, Set(enemy), emptyMessages)
+
+      "Set the highest positioned player to be the actor with a higher position" in {
+        assert(updEnemies.maxBy(_.position).position > updPlayer.position)
+      }
+
+      "Set the lower position character's position to 50" in {
+        assert(updPlayer.position == 50)
+      }
+
+      "Set the higher position character's position to 100" in {
+        assert(updEnemies.exists(_.position == 100))
+      }
+
+      "Include a game message stating it's the higher positioned users turn" in {
+        assert(updMessages.size == 1)
+        assert(updMessages.head.text == "Quick Enemy's turn!")
+      }
+
+      "updating again" must {
+
+        val (latestPlayer, latestEnemies, latestMessages) =
+          GameOps.update(updPlayer, updEnemies, updMessages)
+
+        "retain the active actor" in {
+          assert(updEnemies.maxBy(_.position).position > updPlayer.position)
+          assert(latestEnemies.maxBy(_.position).position > latestPlayer.position)
+        }
+
+        "Include an additional message with the same text" in {
+          assert(latestMessages.size != updEnemies.size)
+          assert(latestMessages.size == 2)
+          assert(latestMessages.last.text == "Quick Enemy's turn!")
+
+          // Same message
+          assert(latestMessages.toSet.size == 1)
+        }
+
+      }
+
+      "updating after the enemy has been reset" must {
+
+        val es = updEnemies.map(e => e.copy(position = e.position-100))
+        val (latestPlayer, latestEnemies, latestMessages) =
+          GameOps.update(updPlayer, es ,
+            updMessages)
+
+        "change the active actor" in {
+          assert(updEnemies.maxBy(_.position).position > updPlayer.position)
+          assert(latestEnemies.maxBy(_.position).position < latestPlayer.position)
+        }
+
+        "Include a game message stating it's now the Player's turn" in {
+          assert(latestMessages.size == 2)
+          assert(latestMessages.exists(_.text == "Player's turn!"))
+          assert(latestMessages.last.text == "Player's turn!")
+        }
+
+      }
+
+    }
+
   }
 
 }
