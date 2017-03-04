ee5ad0fc564ac023713b5ba1b4c8c72357b2092c
rawtoast <jim@jamesmorris.org>
2017-03-04 19:05:33 +0000

    Additional GameOps tests

--- src/test/scala/chousen/core/GameOpsSpec.scala
+++ src/test/scala/chousen/core/GameOpsSpec.scala
@@ -90,9 +90,9 @@ class GameOpsSpec extends WordSpec {
 
       "updating after the enemy has been reset" must {
 
-        val es = updEnemies.map(e => e.copy(position = e.position-100))
+        val es = updEnemies.map(e => e.copy(position = e.position - 100))
         val (latestPlayer, latestEnemies, latestMessages) =
-          GameOps.update(updPlayer, es ,
+          GameOps.update(updPlayer, es,
             updMessages)
 
         "change the active actor" in {
@@ -108,8 +108,78 @@ class GameOpsSpec extends WordSpec {
 
       }
 
+
+      "two actors reach the position goal" should {
+
+        // Note that the enemy is closer to their next turn
+        val player = Player("Player", speed10Char, position = 90)
+        val enemy = Enemy("Enemy", UUID.randomUUID(), speed10Char, position = 91)
+        val emptyMessages = Seq.empty[GameMessage]
+
+        val (updPlayer, updEnemies, updMessages) =
+          GameOps.update(player, Set(enemy), emptyMessages)
+
+        "the faster actor's position is >= 100" in {
+          assert(updEnemies.head.position == 101)
+        }
+
+        "the other character's position is >= 100" in {
+          assert(updPlayer.position == 100)
+        }
+
+        "the actor with the highest position takes precedence" in {
+          assert(updEnemies.maxBy(_.position).position >= updPlayer.position)
+        }
+
+      }
     }
 
+    "provided with an equal enemy and player" should {
+
+      val player = Player("Player", speed10Char, position = 0)
+      val enemy = Enemy("Quick Enemy", UUID.randomUUID(), speed10Char, position = 50)
+
+      val emptyMessages = Seq.empty[GameMessage]
+      val turnOneCast = GameOps.update(player, Set(enemy), emptyMessages)
+      val turnTwoCast = GameOps.update(turnOneCast._1, turnOneCast._2, turnOneCast._3)
+      val turnThreeCast = GameOps.update(turnTwoCast._1, turnTwoCast._2, turnTwoCast._3)
+      val turnFourCast = GameOps.update(turnThreeCast._1, turnThreeCast._2, turnThreeCast._3)
+
+      def nameOfActive(t: (Player, Set[Enemy], Seq[GameMessage])): String = {
+        val (player, e, _) = t
+
+        val enemy = e.maxBy(_.position)
+
+        if (enemy.position > player.position) enemy.name
+        else player.name
+
+      }
+
+      "have a different actor on the 1st/2nd turn" in assert {
+        nameOfActive(turnOneCast) != nameOfActive(turnTwoCast)
+      }
+
+      "have a different actor on the 3rd/4th turn" in assert {
+        nameOfActive(turnThreeCast) != nameOfActive(turnFourCast)
+      }
+
+      "have a different actor on each following turn" in {
+
+        Range.inclusive(0, 5).foreach { (_: Int) =>
+
+          val player = Player("Player", speed10Char, position = 0)
+          val enemy = Enemy("Quick Enemy", UUID.randomUUID(), speed10Char, position = 50)
+          val turnOneCast = GameOps.update(player, Set(enemy), emptyMessages)
+          val turnTwoCast = GameOps.update(turnOneCast._1, turnOneCast._2, turnOneCast._3)
+          val turnThreeCast = GameOps.update(turnTwoCast._1, turnTwoCast._2, turnTwoCast._3)
+          val turnFourCast = GameOps.update(turnThreeCast._1, turnThreeCast._2, turnThreeCast._3)
+
+          assert(nameOfActive(turnOneCast) != nameOfActive(turnTwoCast))
+          assert(nameOfActive(turnThreeCast) != nameOfActive(turnFourCast))
+        }
+
+      }
+    }
   }
 
 }
